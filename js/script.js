1e075669940652756e15eea8be61c8d235f19a3f
RuggeroCoco <ruggerococo94@gmail.com>
2023-03-16 20:30:03 +0100

    End of project Carosello Mono Array

--- js/script.js
+++ js/script.js
@@ -5,3 +5,62 @@ const imagesArray = [
   "../assets/04.jpg",
   "../assets/05.jpg",
 ];
+
+const scrollImages = document.querySelector(".scroll-images");
+
+for (let i = 0; i < imagesArray.length; i++) {
+  const image = `
+  <div class="container-images">
+   <img class="image" src="${imagesArray[i]}" alt="image-${i}" />
+   </div> `;
+  scrollImages.innerHTML += image;
+}
+
+const images = document.getElementsByClassName("image");
+const prevBtn = document.getElementsByClassName("prev")[0];
+const nextBtn = document.getElementsByClassName("next")[0];
+const mainImage = document.getElementById("main-image");
+let activeItem = 0;
+
+images[activeItem].classList.add("active");
+
+function setActiveImage(activeImage) {
+  images[activeImage].classList.add("active");
+  mainImage.src = imagesArray[activeImage];
+}
+
+function disableImage(activeImage) {
+  activeImage.classList.remove("active");
+}
+
+nextBtn.addEventListener("click", function () {
+  if (activeItem === imagesArray.length - 1) {
+    activeItem = 0;
+    setActiveImage(activeItem);
+    disableImage(images[imagesArray.length - 1]);
+  } else if (activeItem === 0) {
+    activeItem++;
+    setActiveImage(activeItem);
+    disableImage(images[0]);
+  } else {
+    activeItem++;
+    setActiveImage(activeItem);
+    disableImage(images[activeItem - 1]);
+  }
+});
+
+prevBtn.addEventListener("click", function () {
+  if (activeItem === 0) {
+    activeItem = imagesArray.length - 1;
+    setActiveImage(activeItem);
+    disableImage(images[0]);
+  } else if (activeItem === imagesArray.length - 1) {
+    activeItem--;
+    setActiveImage(activeItem);
+    disableImage(images[imagesArray.length - 1]);
+  } else {
+    activeItem--;
+    setActiveImage(activeItem);
+    disableImage(images[activeItem + 1]);
+  }
+});
